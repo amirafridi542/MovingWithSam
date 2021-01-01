14ce96b3e83c8dcc09d1059e3c2215677115360d
Amir <amirlink305@gmail.com>
2021-01-01 22:56:02 +0500

    photo alertView

--- Moving With Sam/Controller/Prof Of Delivery/ProfOfDeliveryVC.swift
+++ Moving With Sam/Controller/Prof Of Delivery/ProfOfDeliveryVC.swift
@@ -20,11 +20,29 @@ class ProfOfDeliveryVC: UIViewController, UICollectionViewDelegate, UICollection
     @IBOutlet weak var addPodBtnView: UIView!
     @IBOutlet weak var addSignatureBtnView: UIView!
     
+    
+    var photoLargeView: UIView = {
+        var view = UIView()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
+    var photoAlertSubView: UIView = {
+        var view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         roundingViews()
+        
+        showLockAlertView()
         // Do any additional setup after loading the view.
     }
     
@@ -61,4 +79,125 @@ class ProfOfDeliveryVC: UIViewController, UICollectionViewDelegate, UICollection
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
     }
+    
+    
+    // MARK: - AlertView
+    private func showLockAlertView()
+    {
+        self.view.addSubview(photoLargeView)
+        self.view.bringSubviewToFront(photoLargeView)
+        photoLargeView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
+        photoLargeView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
+        photoLargeView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
+        photoLargeView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
+        view.addSubview(self.photoLargeView)
+        self.photoLargeView.addSubview(photoAlertSubView)
+        
+        photoAlertSubView.backgroundColor = UIColor(red: 0.47, green: 0.47, blue: 0.47, alpha: 1.00)
+        photoAlertSubView.translatesAutoresizingMaskIntoConstraints = false
+        photoAlertSubView.leadingAnchor.constraint(equalTo: photoLargeView.leadingAnchor, constant: 30).isActive =  true
+        photoAlertSubView.trailingAnchor.constraint(equalTo: photoLargeView.trailingAnchor, constant: -30).isActive =  true
+        photoAlertSubView.centerYAnchor.constraint(equalTo: photoLargeView.centerYAnchor).isActive = true
+        photoAlertSubView.heightAnchor.constraint(equalToConstant: 200.0).isActive = true
+        photoAlertSubView.layer.cornerRadius = 10
+        
+        let crossBtn  = UIButton()
+        crossBtn.translatesAutoresizingMaskIntoConstraints = false
+        crossBtn.setImage(UIImage(named: "cross"), for: .normal)
+        crossBtn.addTarget(self, action: #selector(hidePhotoAlertView), for:.touchUpInside)
+        
+        photoAlertSubView.addSubview(crossBtn)
+        
+        crossBtn.trailingAnchor.constraint(equalTo: photoAlertSubView.trailingAnchor, constant: -24).isActive =  true
+        crossBtn.topAnchor.constraint(equalTo: photoAlertSubView.topAnchor, constant: 16).isActive = true
+        crossBtn.widthAnchor.constraint(equalToConstant: 49).isActive = true
+        crossBtn.heightAnchor.constraint(equalToConstant: 52).isActive = true
+        
+        
+        let cameraBtn  = UIButton()
+        cameraBtn.translatesAutoresizingMaskIntoConstraints = false
+        cameraBtn.setImage(UIImage(named: "chooseCamera"), for: .normal)
+        cameraBtn.addTarget(self, action: #selector(hidePhotoAlertView), for:.touchUpInside)
+        
+        photoAlertSubView.addSubview(cameraBtn)
+        
+        cameraBtn.leadingAnchor.constraint(equalTo: photoAlertSubView.leadingAnchor, constant: 24).isActive =  true
+        cameraBtn.topAnchor.constraint(equalTo: photoAlertSubView.topAnchor, constant: 46).isActive = true
+        cameraBtn.widthAnchor.constraint(equalToConstant: 55).isActive = true
+        cameraBtn.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        
+        
+        let takePhotoBtn  = UIButton()
+        takePhotoBtn.translatesAutoresizingMaskIntoConstraints = false
+        takePhotoBtn.addTarget(self, action: #selector(hidePhotoAlertView), for:.touchUpInside)
+        takePhotoBtn.titleLabel?.font =  UIFont(name:"Poppins-Regular", size: 14)
+        takePhotoBtn.setTitleColor(UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0), for: UIControl.State.normal)
+        takePhotoBtn.setTitle("Take Photo", for: UIControl.State.normal)
+        
+        photoAlertSubView.addSubview(takePhotoBtn)
+        
+      //  takePhotoBtn.topAnchor.constraint(equalTo: photoAlertSubView.topAnchor, constant: 46).isActive =  true
+        
+        takePhotoBtn.centerYAnchor.constraint(equalTo: cameraBtn.centerYAnchor, constant: 0).isActive  = true
+        takePhotoBtn.trailingAnchor.constraint(equalTo: photoAlertSubView.trailingAnchor, constant: -24).isActive =  true
+        takePhotoBtn.leadingAnchor.constraint(equalTo: photoAlertSubView.leadingAnchor, constant: 10).isActive =  true
+        takePhotoBtn.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        
+        let separatorView  = UIView()
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        separatorView.backgroundColor = UIColor.lightGray
+        photoAlertSubView.addSubview(separatorView)
+        separatorView.leadingAnchor.constraint(equalTo: photoAlertSubView.leadingAnchor, constant: 24).isActive =  true
+        separatorView.trailingAnchor.constraint(equalTo: photoAlertSubView.trailingAnchor, constant: -24).isActive =  true
+
+        separatorView.topAnchor.constraint(equalTo: takePhotoBtn.bottomAnchor, constant: 16).isActive = true
+        separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        
+        let galleryBtn  = UIButton()
+        galleryBtn.translatesAutoresizingMaskIntoConstraints = false
+        galleryBtn.setImage(UIImage(named: "gallery"), for: .normal)
+        galleryBtn.addTarget(self, action: #selector(hidePhotoAlertView), for:.touchUpInside)
+
+        photoAlertSubView.addSubview(galleryBtn)
+
+        galleryBtn.leadingAnchor.constraint(equalTo: photoAlertSubView.leadingAnchor, constant: 32).isActive =  true
+        galleryBtn.topAnchor.constraint(equalTo: separatorView.bottomAnchor, constant: 15).isActive = true
+        galleryBtn.widthAnchor.constraint(equalToConstant: 35).isActive = true
+        galleryBtn.heightAnchor.constraint(equalToConstant: 35).isActive = true
+
+
+
+        let chooseGalleryBtn  = UIButton()
+        chooseGalleryBtn.translatesAutoresizingMaskIntoConstraints = false
+        chooseGalleryBtn.addTarget(self, action: #selector(hidePhotoAlertView), for:.touchUpInside)
+        chooseGalleryBtn.titleLabel?.font =  UIFont(name:"Poppins-Regular", size: 14)
+        chooseGalleryBtn.setTitleColor(UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0), for: UIControl.State.normal)
+        chooseGalleryBtn.setTitle("Choose from gallery", for: UIControl.State.normal)
+
+        photoAlertSubView.addSubview(chooseGalleryBtn)
+
+        chooseGalleryBtn.topAnchor.constraint(equalTo: separatorView.bottomAnchor, constant: 15).isActive =  true
+        chooseGalleryBtn.trailingAnchor.constraint(equalTo: photoAlertSubView.trailingAnchor, constant: -24).isActive =  true
+        chooseGalleryBtn.leadingAnchor.constraint(equalTo: galleryBtn.leadingAnchor, constant: 15).isActive =  true
+        chooseGalleryBtn.heightAnchor.constraint(equalToConstant: 30).isActive = true
+
+        
+    }
+    
+    @objc func hidePhotoAlertView()
+    {
+        self.photoLargeView.removeFromSuperview()
+        //statusValue  = 0
+        //self.createShiftLargeView.removeFromSuperview()
+    }
+    @objc func hideLockAlertView()
+    {
+        self.photoLargeView.removeFromSuperview()
+        //statusValue  = 0
+        //self.createShiftLargeView.removeFromSuperview()
+    }
+    
+    
 }  // end class
