d937abd2d8b60ca1e79d477d993730c053e2e527
Amir <amirlink305@gmail.com>
2021-01-02 09:24:45 +0500

    assign jobVc add

---
+++ Moving With Sam/Controller/Assign job/AssignJobCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  AssignJobCollectionCell.swift
+//  Moving With Sam
+//
+//  Created by Amir Khan on 02/01/2021.
+//
+
+import UIKit
+
+class AssignJobCollectionCell: UICollectionViewCell {
+    
+}
--- Moving With Sam/Controller/Assign job/AssignJobVC.swift
+++ Moving With Sam/Controller/Assign job/AssignJobVC.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
-class AssignJobVC: UIViewController {
+class AssignJobVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var curveView : UIView!
     @IBOutlet weak var searchView : UIView!
 
-    
+    @IBOutlet weak var assignCollectionView : UICollectionView!
+
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +22,30 @@ class AssignJobVC: UIViewController {
         searchView.layer.cornerRadius = 10.0;
         searchView.clipsToBounds = true
         
+        
+        assignCollectionView.delegate = self
+        assignCollectionView.dataSource = self
+
         // Do any additional setup after loading the view.
     }
+    
+    
+    // MARK : - Collection Delegate Method
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return 14
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier:"AssignJobCollectionCell", for: indexPath) as! AssignJobCollectionCell
+            
+            return cell
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            
+        }
+        
+        
     
 } // end class
 
